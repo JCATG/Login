db975153b925eb7c9ccb5dd30d4713a0c69a5543
JCATG <91666540+JCATG@users.noreply.github.com>
2022-01-10 15:04:40 -0300

    criado o login

---
+++ login/script/script.js
@@ -0,0 +1,20 @@
+var email= document.getElementById('email')
+var senha = document.getElementById('senha')
+var env = document.getElementById('env')
+
+function validar(){
+  var nome = formulario.nome;
+  if (nome.value == ""){
+      alert("preencha o campo a cima");
+
+      nome.focus();
+  }
+  else if(senha.value ==""){
+    alert("informe a senha para entrar")
+    senha.focus();
+  }
+  else if ((nome.value == "j.alves160600@gmail.com") && (senha.value == "12345678")) {
+    alert("login realizado")
+  }
+ 
+}
